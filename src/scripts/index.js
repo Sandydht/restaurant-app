eba8147bcd2441365e3a6bdef59db535e2ac3e76
Sandydht <sandydwi75@gmail.com>
2021-05-17 21:28:18 +0700

    final accesibility

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,19 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+import '../styles/responsive.scss';
 
-console.log('Hello Coders! :)');
+const menuButton = document.getElementById('menuButton');
+const navbar = document.querySelector('.app-bar__nav');
+
+menuButton.addEventListener('click', event => {
+  navbar.classList.toggle('open');
+  event.stopPropagation();
+});
+
+document.body.addEventListener('click', event => {
+  navbar.classList.remove('open');
+  event.stopPropagation();
+});
+
+const footerYear = document.getElementById('footer__year');
+footerYear.innerHTML = new Date().getFullYear();

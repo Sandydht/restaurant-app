66de4c914edf69f9183ad2f517b8eba35ca7815d
Sandydht <sandydwi75@gmail.com>
2021-06-04 15:48:15 +0700

    web component

--- src/scripts/views/templates/templates-creator.js
+++ src/scripts/views/templates/templates-creator.js
@@ -1,28 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
 
-const createRestaurantAddCustomerReviewTemplate = () => `
-  <div class="input__name">
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" placeholder="Your name...">
-  </div>
-
-  <div class="input__review">
-    <label for="review">Review</label>
-    <textarea type="text" name="review" id="review" rows="5"  placeholder="Your review..."></textarea>
-  </div>
-
-  <button id="submitReview">Submit</button>
-`;
-
-const createRestaurantReviewItemTemplate = (customerReviews) => `
-  <div class="review__item">
-    <p class="name">Name: ${customerReviews.name}</p>
-    <p class="date">Date: ${customerReviews.date}</p>
-    <p class="review">Review: ${customerReviews.review}</p>
-  </div>
-`;
-
 const createLikeButtonTemplate = () => `
   <button aria-label="like this movie" id="likeButton" class="like">
     <i class="fa fa-heart-o" aria-hidden="true"></i>
@@ -36,8 +14,6 @@ const createLikedButtonTemplate = () => `
 `;
 
 export {
-  createRestaurantAddCustomerReviewTemplate,
-  createRestaurantReviewItemTemplate,
   createLikeButtonTemplate,
   createLikedButtonTemplate,
 };

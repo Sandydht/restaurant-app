b6fb3028168bb31ee9bcd378da052018549e3c92
Sandydht <sandydwi75@gmail.com>
2021-06-05 21:52:45 +0700

    Final dev 2.1

--- src/scripts/views/component/app-drawer/index.js
+++ src/scripts/views/component/app-drawer/index.js
@@ -35,8 +35,8 @@ class AppDrawer extends HTMLElement {
 
       <nav class="app-bar__nav">
         <ul class="nav__list">
-          <li class="nav__item"><a href="#/home">Home</a></li>
-          <li class="nav__item"><a href="#/favorite">Favorite</a></li>
+          <li class="nav__item"><a href="/#/home">Home</a></li>
+          <li class="nav__item"><a href="/#/favorite">Favorite</a></li>
           <li class="nav__item"><a href="https://www.linkedin.com/in/sandy-dwi-handoko-trapsilo-a56008185" target="_blank" rel="noreferrer">About Us</a></li>
         </ul>
       </nav>
